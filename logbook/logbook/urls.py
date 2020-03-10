59c48aef7292cde734c4c244f11d3f7269cada60
Viktor <vsybrex@gmail.com>
2020-03-10 17:37:19 +0200

    Basic styling

--- logbook/logbook/urls.py
+++ logbook/logbook/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 
@@ -24,3 +26,8 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
+
+# serving media and static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
