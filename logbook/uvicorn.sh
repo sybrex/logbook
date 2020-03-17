4c99570f52b764636671de19e3858b56c7717850
Viktor <vsybrex@gmail.com>
2020-03-17 15:01:23 +0100

    Set port for web app

--- logbook/uvicorn.sh
+++ logbook/uvicorn.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
-pipenv run uvicorn --port=8001 logbook.asgi:application --reload
+pipenv run uvicorn --port=8002 logbook.asgi:application --reload
